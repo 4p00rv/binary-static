4fee858aaa04b0b974d63a3d6a89a3794a4bb8cd
Apoorv Joshi <apoorv@binary.com>
2019-12-19 18:09:57 +0800

    add grunt config for git

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -19,6 +19,10 @@ module.exports = function (grunt) {
                 base   : 'dist',
                 branch : 'temp',
                 repo   : 'git@github.com:4p00rv/temp-test',
+                user   : !grunt.option('ci') ? {} : {
+                  name  : 'CircleCI',
+                  email : 'sysadmin@binary.com'
+                },
                 message: 'Trigger tests',
             },
             src: grunt.option('staging') ? 'version' : '',
